2b04b113848311c1fd976e12c2fa4ad4ca8e3f17
Shay Rojansky <roji@roji.org>
2021-02-13 18:47:48 +0100

    Add option to disable concurrency detection

---
+++ test/EFCore.InMemory.FunctionalTests/ConcurrencyDetectorEnabledInMemoryTest.cs
@@ -0,0 +1,24 @@
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Microsoft.EntityFrameworkCore.TestUtilities;
+using Xunit;
+
+namespace Microsoft.EntityFrameworkCore
+{
+    [Collection("ConcurrencyDetector")]
+    public class ConcurrencyDetectorEnabledInMemoryTest : ConcurrencyDetectorEnabledTestBase<
+        ConcurrencyDetectorEnabledInMemoryTest.ConcurrencyDetectorInMemoryFixture>
+    {
+        public ConcurrencyDetectorEnabledInMemoryTest(ConcurrencyDetectorInMemoryFixture fixture)
+            : base(fixture)
+        {
+        }
+
+        public class ConcurrencyDetectorInMemoryFixture : ConcurrencyDetectorFixtureBase
+        {
+            protected override ITestStoreFactory TestStoreFactory
+                => InMemoryTestStoreFactory.Instance;
+        }
+    }
+}
---
+++ test/EFCore.Sqlite.FunctionalTests/ConcurrencyDetectorDisabledSqliteTest.cs
@@ -0,0 +1,30 @@
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Microsoft.EntityFrameworkCore.TestUtilities;
+using Xunit;
+
+namespace Microsoft.EntityFrameworkCore
+{
+    [Collection("ConcurrencyDetector")]
+    public class ConcurrencyDetectorDisabledSqliteTest : ConcurrencyDetectorDisabledRelationalTestBase<
+        ConcurrencyDetectorDisabledSqliteTest.ConcurrencyDetectorSqlServerFixture>
+    {
+        public ConcurrencyDetectorDisabledSqliteTest(ConcurrencyDetectorSqlServerFixture fixture)
+            : base(fixture)
+        {
+        }
+
+        public class ConcurrencyDetectorSqlServerFixture : ConcurrencyDetectorFixtureBase
+        {
+            protected override ITestStoreFactory TestStoreFactory
+                => SqliteTestStoreFactory.Instance;
+
+            public TestSqlLoggerFactory TestSqlLoggerFactory
+                => (TestSqlLoggerFactory)ListLoggerFactory;
+
+            public override DbContextOptionsBuilder AddOptions(DbContextOptionsBuilder builder)
+                => builder.DisableConcurrencyDetection();
+        }
+    }
+}
